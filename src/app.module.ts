a53107fc657693ac6acd4a1f4bbd8154baedae67
NESTOR <mass.nestor@gmail.com>
2022-11-16 12:19:01 -0500

    se agrega nuevo common module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
     //Conexion con la base de datos
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot('mongodb+srv://massnode:d3vdSr6pZWVuSXew@cluster0.jypzl2s.mongodb.net/Cluster0'),
     PokemonModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
